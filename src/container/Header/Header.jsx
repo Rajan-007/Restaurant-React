86b2976d9f692f11d5fdb6dba38804be96d2c0cb
Rajan-007 <aravinthrajan034@gmail.com>
2023-04-22 22:54:49 +0530

    Front page

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import './Header.css';
+import SubHeading from '../../components/SubHeading/SubHeading';
+import images from '../../constants/images';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding'id='home'>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className='app__header-h1'>The key to fine Dining</h1>
+      <p className='p__opensans' style={{margin : "2rem 0" }}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse officia ab est? Cum, possimus. Consequatur odio fuga praesentium iure explicabo! </p>
+      <button type='button'className='custom__button'>Explore menu</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt='header img' />
+    </div>
   </div>
 );
 
