72cd52fbc6be46cd90cb024afc8337d52a9dfc29
Rajan-007 <aravinthrajan034@gmail.com>
2023-04-23 22:17:39 +0530

    Chef page added

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import {images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wrapper_img_reverse">
+      <img src={images.chef} alt='chef' />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="chef's world " />
+      <h1 className='headtext__cormorant'>what we belive in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt='quote' />
+          <p className='p__opensans'>Lorem ipsum dolor, sit ame dolorum ratione hic quibusdam nam temporibus quia esse laudantium a , obcaecati dicta!</p>
+        </div>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam omnis animi temporibus molestias laborum a assumenda repudiandae nulla eaque</p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
