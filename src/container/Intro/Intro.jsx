7b7fc0b4c82015f6d549da1a3ea7ee48637d8285
Rajan-007 <aravinthrajan034@gmail.com>
2023-04-24 18:21:36 +0530

    video section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -5,20 +5,32 @@ import{meal} from '../../constants'
 import './Intro.css';
 
 const Intro = () => {
-  const [setPlay ,setPlayVideo ] = useState(false)
+  const [playVideo ,setPlayVideo ] = React.useState(false)
   const vidRef = React.useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if(playVideo) {
+      vidRef.current.pause();
+    } else{
+      vidRef.current.play();
+    }
+  }
     return(
       <div className='app__video'>
         <video
           src={meal } 
           ref={vidRef} 
           type='video/mp4'
-          loop
+          Loop
           controls={false}
           muted
         />
          <div className="app__video-overlay flex__center">
-          <div className="app__video-overlay_circle flex__center">
+          <div className = "app__video-overlay_circle flex__center" onClick={handleVideo}>
+            {playVideo ? (<BsPauseFill color ='#fff' font-size={30} />)
+            : <BsFillPlayFill color='#fff' fontSize={30} />}
 
           </div>
          </div>
